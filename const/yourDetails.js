6c96c31d7b7fcdcd69cec0a8b5dcbd799913d099
ChristianOConnor <warpanomaly@gmail.com>
2022-12-13 04:07:14 -0500

    add yourDetails file

---
+++ const/yourDetails.js
@@ -0,0 +1,5 @@
+// Replace this with your contract address
+export const contractAddress = process.env.CONTRACT_ADDRESS;
+
+// Replace this with your domain name.
+export const domainName = "localhost";
