ffb53bbc455426368daae8c6ff7bc02474238a78
ChristianOConnor <47836708+ChristianOConnor@users.noreply.github.com>
2022-12-17 04:47:03 -0500

    Update checkBalance.js

--- util/checkBalance.js
+++ util/checkBalance.js
@@ -1,8 +1,8 @@
 import { contractAddress } from "../const/yourDetails";
 
 export default async function checkBalance(sdk, address) {
-  const editionDrop = sdk.getEditionDrop(
-    contractAddress, // replace this with your contract address
+  const editionDrop = sdk.useContract(
+    contractAddress, contractAbi //pass your custom contract's abi in this function as an argument
   );
 
   const balance = await editionDrop.balanceOf(address, 0);
